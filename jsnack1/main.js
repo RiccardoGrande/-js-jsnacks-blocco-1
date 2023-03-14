1b3416fa8b19fe89aece08f95b0d0acf4da4545c
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-14 11:42:51 +0100

    add complete exercise

---
+++ jsnack1/main.js
@@ -0,0 +1,18 @@
+const numberFirst = prompt('inserisci un numero')
+console.log(numberFirst);
+
+const numberSecond = prompt('inserisci un numero')
+console.log(numberSecond);
+
+if (numberFirst > numberSecond) {
+     
+    console.log(numberFirst);
+
+} else if (numberFirst < numberSecond) {
+    
+     console.log(numberSecond);
+
+} else {
+                       
+    console.log('there is no major number');
+}
