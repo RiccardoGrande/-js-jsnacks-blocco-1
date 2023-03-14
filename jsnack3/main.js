934983857c76d8fd48e7d96b11e229c544d9d602
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-14 12:51:43 +0100

    add exercise

---
+++ jsnack3/main.js
@@ -0,0 +1,14 @@
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+
+let numberRequest = prompt('inserisci un numero') 
+
+console.log(numberRequest);
+
+
+
+
+//Il programma stampa la somma di tutti i numeri inseriti.
+
+for (let i = 0; i < 10; i++) {
+ console.log (numberRequest[i]);
+}
